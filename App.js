ea49ba5eca0bf43df154944ca27e03afdbdc0545
Aiky <aikllmy13@gmail.com>
2026-05-07 20:36:56 -0300

    descrição da mudança

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import AppNavigator from './src/navigation/AppNavigator';
+import { DatabaseInit } from './src/database/DatabaseInit';
 
 export default function App() {
+  useEffect(() => {
+    try {
+      DatabaseInit();
+      console.log("Banco de dados Cafeteste pronto!");
+    } catch (error) {
+      console.error("Erro ao iniciar o banco de dados:", error);
+    }
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <AppNavigator />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
